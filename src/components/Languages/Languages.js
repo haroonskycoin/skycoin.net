f092326c399577f9cc8384226446ae245028d521
Ivan Payan <ivanpayanm@gmail.com>
2019-02-06 14:44:57 +0100

    fixed clicable area

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -16,6 +16,7 @@ const Language = styled.span`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
+  line-height:50px;
 
   &:hover {
     text-decoration: underline;
@@ -26,23 +27,8 @@ export default () => (
   <Languages>
     <Language><StyledLink to="/">English</StyledLink></Language>
     <Language><StyledLink to="/fr/">Français</StyledLink></Language>
-    <Flag
-      name="chinese"
-      render={() => (
-        <Language><StyledLink to="/zh/">中文</StyledLink></Language>
-      )}
-    />
-    <Flag
-      name="japanese"
-      render={() => (
-        <Language><StyledLink to="/ja/">日本語</StyledLink></Language>
-      )}
-    />
-    <Flag
-      name="korean"
-      render={() => (
-        <Language><StyledLink to="/ko/">한국어</StyledLink></Language>
-      )}
-    />
+    <Language><StyledLink to="/zh/">中文</StyledLink></Language>
+    <Language><StyledLink to="/ja/">日本語</StyledLink></Language>
+    <Language><StyledLink to="/ko/">한국어</StyledLink></Language>
   </Languages>
 );
