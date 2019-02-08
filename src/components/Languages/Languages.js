fa4bdc85d931e744bc37de33575e94a58037e1a7
Ivan Payan <ivanpayanm@gmail.com>
2019-02-08 10:20:47 +0100

    fixed lint problems

--- src/components/Languages/Languages.js
+++ src/components/Languages/Languages.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { Flag } from 'flag';
 
 const Languages = styled.div`
   list-style: none;
