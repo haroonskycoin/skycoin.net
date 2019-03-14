7135bf44b760c96ed6ca0866c27bbf6423ec39e9
Ivan <ivanpayanm@gmail.com>
2019-03-14 12:23:56 +0100

    fixed lint problems

---
+++ src/components/Downloads/sort.js
@@ -0,0 +1,9 @@
+import { isMobile } from 'react-device-detect';
+
+
+const sortByMobile = list => list.sort((a, b) => {
+  if (a.mobile === b.mobile) return 0;
+  if (isMobile) return a.isMobile ? 1 : -1;
+  return a.isMobile ? -1 : 1;
+});
+export default { sortByMobile };
